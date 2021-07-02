1aa51f09dbd1e87e81ec3228153f573d50dc07dc
k2moons <moonsocean@gmail.com>
2021-07-03 01:09:26 +0900

    add example

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["BwCalendar"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/BlueEventHorizon/BwTools.git", from: "3.0.4"),
+        .package(url: "https://github.com/BlueEventHorizon/BwTools.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
