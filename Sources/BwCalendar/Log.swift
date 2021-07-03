ca6ab300fc53b1374e24b70622b2b42719538a10
k2moons <moonsocean@gmail.com>
2021-07-03 16:25:42 +0900

    rename file for log instance

---
+++ Sources/BwCalendar/Log.swift
@@ -0,0 +1,13 @@
+//
+//  Log.swift
+//  BwTools
+//
+//  Created by k2moons on 2021/06/26.
+//  Copyright (c) 2018 k2moons. All rights reserved.
+//
+import BwLogger
+import Foundation
+import os
+
+// swiftlint:disable:next file_types_order prefixed_toplevel_constant
+internal let log = Logger(PrintLogger())
